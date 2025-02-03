c14a5c05e390714c8ba3dc6842968a161d4d5247
Lou Xuan Dio <louxuan@bu.edu>
2025-02-03 00:46:24 -0500

    add Row Composable for a star icon

--- app/src/main/java/com/example/recipemeatball/MainActivity.kt
+++ app/src/main/java/com/example/recipemeatball/MainActivity.kt
@@ -1,40 +1,34 @@
 package com.example.recipemeatball
 
 import android.os.Bundle
-import android.text.Layout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.recipemeatball.ui.theme.RecipeMeatballTheme
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.background
 import androidx.compose.material3.Card
-import androidx.compose.material3.Divider
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.ui.unit.Dp
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.HorizontalDivider
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-        setContent(){
+        setContent{
             RecipeMeatballTheme {
 
                 Column (
@@ -44,18 +38,27 @@ class MainActivity : ComponentActivity() {
                         .fillMaxSize()
                         .padding(16.dp)
                 ) {
-                    Text(
-                        "Crock Pot Party Meatballs",
-                        modifier = Modifier
-                            .align(Alignment.Start)
-                            .padding(bottom = 8.dp),
-                        style = TextStyle(
-                            fontSize = 24.sp,
-                            fontWeight = FontWeight.Bold
+                    Row (
+                        modifier = Modifier.padding(horizontal = 16.dp)
+                    ){
+                        Text(
+                            "Crock Pot Party Meatballs",
+                            modifier = Modifier
+                                .padding(bottom = 8.dp)
+                                .weight(1f),
+                            style = TextStyle(
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold
+                            )
                         )
+                        Icon(
+                            imageVector = Icons.Filled.Star,
+                            contentDescription = "Save Button",
+                            tint = Color.Gray
+                        )
+                    }
 
-                    )
-                    Divider(
+                    HorizontalDivider(
                         modifier = Modifier
                             .fillMaxWidth(),
                         color = Color.Gray,
@@ -86,8 +89,8 @@ class MainActivity : ComponentActivity() {
                                 .padding(8.dp),
                             verticalArrangement = Arrangement.spacedBy(6.dp)
                         ) {
-                            Text(
-                                "Ingredients:",
+
+                            Text("Ingredients:",
                                 style = TextStyle(
                                     fontSize = 20.sp,
                                     fontWeight = FontWeight.Bold
